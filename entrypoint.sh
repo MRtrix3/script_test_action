ec4002036aadd3be20924edaac5bc9cb21396d4f
Robert Smith <robert.smith@florey.edu.au>
2020-09-21 13:20:47 +1000

    Entrypoint: Temporarily point to MRtrix3 forked repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Git commitish to be checkout out must be provided as first argument on the command-line
-cmd="git clone -b $1 --depth 1 https://github.com/MRtrix3/mrtrix3.git ."
+cmd="git clone -b $1 --depth 1 https://github.com/Lestropie/mrtrix3.git ."
 echo $cmd; eval $cmd
-#git clone -b $1 --depth 1 https://github.com/MRtrix3/mrtrix3.git .
+#git clone -b $1 --depth 1 https://github.com/Lestropie/mrtrix3.git .
 ./configure -assert -nogui
 ./build
 ./run_tests scripts || ( cat testing_scripts.log && exit 1 )
